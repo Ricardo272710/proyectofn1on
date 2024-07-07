089c9a096e91bce353315fde8e149c042a62e2af
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-07 16:29:05 -0500

    Update anthony.js

--- plugins/anthony.js
+++ plugins/anthony.js
@@ -2,13 +2,13 @@
 
 let handler = async (m, { conn, text}) => {
 
-m.reply(`╭┄〔 *𝗧𝗶𝗺𝗲 Spider 𝗕𝗼𝘁* 〕┄⊱\n┊\n┊ *${pickRandom(global.anuel)}*\n┊\n╰━⊰ Anthony ⊱━დ`)
+m.reply(`╭┄〔 *𝗧𝗶𝗺𝗲 Spider 𝗕𝗼𝘁* 〕┄⊱\n┊\n┊ *${pickRandom(global.anuel)}*\n┊\n╰━⊰ Ricardo ⊱━დ`)
 }
 handler.tags = ['humor']
-handler.command = ['anthony']
+handler.command = ['ricardo']
 export default handler
 
 function pickRandom(list) {
 return list[Math.floor(list.length * Math.random())]}
 
-global.anthony = ["¿Se te olvidó que tú y yo somos fueguito🔥y me lo chupaste en tu cuarto?"]
+global.ricardo = ["¿Se te olvidó que tú y yo somos fueguito🔥y me lo chupaste en tu cuarto?"]
